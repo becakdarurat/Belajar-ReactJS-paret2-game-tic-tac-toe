897c92afe2aad329d9b272943e2208f8672fa52c
becakdarurat <sofyant757@gmail.com>
2023-11-17 21:04:36 +0700

    saya belajar props , data yang dikirim dari components induk ke anak.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from 'react'
 
-function Square(){
-  return <button className='square'>X</button>
+function Square({ value, onSquareClick }){
+
+        // kita komentari karena tidak di pakai lagi, karena nilai dari board nya sudah di naikkan
+  // kita buat statenya, awalnya string kosaong
+  // const [value, setValue] = useState('');
+
+  // kita buat sebuat event ketika diclick , kita setvaluenya
+  // function handleClick(){
+  //   setValue('X');
+  // }
+
+
+  return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
-function App() {
+export default function Board() {
+  // kita buat state, jad setiap function itu bisa punya state
+  // kita bikin array 9, lalu isi nilai awalnya null
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(){
+    // kita buat variable baru,tidak pakai variable sudah ada karena nanti akan kepake saat buat time travel
+
+  }
 
   return (
-    <>
-      <Square/>
-    </>
+    <div className='board'>
+      
+{/* Ya, value pada kode tersebut dapat disebut dengan props di React JS. Props adalah data yang dikirim dari komponen induk ke komponen anak. Komponen anak dapat menggunakan props untuk menentukan bagaimana mereka harus dirender. */}
+      <Square value={squares[0]} onSquareClick={handleClick(0)} />
+      <Square value={squares[1]} />
+      <Square value={squares[2]} />
+      <Square value={squares[3]} />
+      <Square value={squares[4]} />
+      <Square value={squares[5]} />
+      <Square value={squares[6]} />
+      <Square value={squares[7]} />
+      <Square value={squares[8]} />
+    </div>
   )
 }
-
-export default App
+// kita pindahkan ke atas biar lebih singkat dalam penulisan syntax nya
+// export default App 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// gk usah khwatir walaupun nama componentnya kita buat board, tetap yang di compile nama filenya
 import App from './App.jsx'
 import './index.css'
 
